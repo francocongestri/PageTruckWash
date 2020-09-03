1df9c3b0f97af430934e8f5fe0b41cc3f7043255
Franco_Congestri <francoacongestri@gmail.com>
2020-09-02 21:35:24 -0300

    Cambios adivinanza

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,29 @@
 // Empty JS for your own code to be here
+function checkAnswer(){
+    if (precio == 333){
+            ganaste = true;
+            alert("Ganaste!");
+        }
+        else {
+            intentos++
+            if (intentos == 1){
+                alert("Incorrecto \n intenta nuevamente");
+            }
+            else if(intentos == 2){
+                alert("Incorrecto \n Es un numero de cuatro cifras");
+            }
+            else if(intentos == 3){
+                alert("Incorrecto \n En la pagina esta escrito el precio");
+            }
+            else if(intentos == 4){
+                alert("Perdiste");
+            }
+        }
+}
+function main(){
+    var intentos = 0;
+    while ((intentos < 4) && (ganaste == false)){
+        document.getElementById("precio").submit();
+        checkAnswer();
+    }
+}    
