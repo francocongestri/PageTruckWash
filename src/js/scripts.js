863a662e3b56fbbfebe54506297bbe7c157b209d
Franco_Congestri <francoacongestri@gmail.com>
2020-09-03 19:06:25 -0300

    correciones adivinanza

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
-// Empty JS for your own code to be here
-function checkAnswer(){
-    if (precio == 333){
+//Funciones adivinanza
+/*function checkAnswer(){
+    if (document.getElementById("question0") == 1113){
             ganaste = true;
             alert("Ganaste!");
         }
@@ -19,11 +19,31 @@ function checkAnswer(){
                 alert("Perdiste");
             }
         }
-}
+}*/
 function main(){
     var intentos = 0;
+    var ganaste = false;
     while ((intentos < 4) && (ganaste == false)){
-        document.getElementById("precio").submit();
-        checkAnswer();
+        document.getElementById("adivinanza")
+        if (document.getElementById("question0") == 1113){
+            ganaste = true;
+            alert("Ganaste!");
+        }
+        else {
+            intentos++
+            if (intentos == 1){
+                alert("Incorrecto! \n intenta nuevamente");
+            }
+            else if(intentos == 2){
+                alert("Incorrecto! \n Es un numero de cuatro cifras");
+            }
+            else if(intentos == 3){
+                alert("Incorrecto! \n En la pagina esta escrito el precio");
+            }
+            else if(intentos == 4){
+                alert("Perdiste!");
+            }
+        }
     }
 }    
+//Fin funciones adivinanza
